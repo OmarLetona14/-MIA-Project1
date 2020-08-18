0d7744f7c32fdfd1b0efc434a35e95f5d7ffc1b8
OmarLetona14 <omarletonaf@gmail.com>
2020-08-18 01:03:47 -0600

    Added partitions command

--- function/binaryFile.go
+++ function/binaryFile.go
@@ -6,22 +6,64 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"unsafe"
+	"math/rand"
+	"time"
 )
 
-func ReadBinaryFile(file_name string) {
+type mbr struct{
+	Size int64
+	Time[25] byte
+	Disk_signature int8
+	Partitions[3] partition
+}
+
+type partition struct{
+	Status byte
+	Type byte 
+	Fit byte
+	Start int64
+	Size int64
+	Name[16] byte
+
+}
+
+func ReadBinaryFile(file_name string) mbr {
 	file, err := os.Open(file_name)
 	defer file.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	var m int8
-	data := ReadNextBytes(file, 16)
+	var m mbr
+	var mbr_size int = int(unsafe.Sizeof(m)) + 1 
+	data := ReadNextBytes(file, mbr_size)
 	buffer := bytes.NewBuffer(data)
 	err = binary.Read(buffer, binary.BigEndian, &m)
 	if err != nil {
 		log.Fatal("binary.Read failed", err)
 	}
-	fmt.Println(m)
+	return m
+}
+
+func printDisk(m mbr){
+	fmt.Println("Disk size", m.Size)
+	myString := string(m.Time[:])
+	fmt.Println("Disk created at", myString)
+	fmt.Println("Disk signature", m.Disk_signature)
+	for i := 0; i < len(m.Partitions); i++ {
+		par := m.Partitions[i]
+		fmt.Println("------------------------------------------------------------------")
+		fmt.Println("Partition", i)
+		fmt.Println("Partition status", par.Status)
+		fmt.Println("Partition type", par.Type)
+		fmt.Println("Partition fit", par.Fit)
+		fmt.Println("Partition start",par.Start)
+		fmt.Println("Partition size",par.Size)
+		parName := string(par.Name[:])
+		fmt.Println("Partition name",parName)
+		fmt.Println("------------------------------------------------------------------")
+	}
+
 }
 
 func ReadNextBytes(file *os.File, number int) []byte {
@@ -33,7 +75,7 @@ func ReadNextBytes(file *os.File, number int) []byte {
 	return bytes
 }
 
-func WriteBinaryFile(file_name string, file_path string, file_size int64) {
+func CreateBinaryFile(file_name string, file_path string, file_size int64) {
 	file_route := file_path + file_name
 	file, err := os.Create(file_route)
 	defer file.Close()
@@ -43,13 +85,44 @@ func WriteBinaryFile(file_name string, file_path string, file_size int64) {
 	}
 	var cero int64 = 0
 	s := &cero
-	var bin_buf bytes.Buffer
-	binary.Write(&bin_buf, binary.BigEndian, s)
-	WriteNextBytes(file, bin_buf.Bytes())
+	//var bin_buf bytes.Buffer
+	//binary.Write(&bin_buf, binary.BigEndian, s)
+	//WriteNextBytes(file, bin_buf.Bytes())
+	var mbrs mbr
+	mbrs.Size = file_size
+	mbrs.Disk_signature = generateRandom()
+	dt := time.Now()
+	tiempo := dt.Format("01-02-2006 15:04:00")
+	copy(mbrs.Time[:],tiempo)
+	for i:=0;i<len(mbrs.Partitions);i++ {
+		mbrs.Partitions[i].Status = '0'
+	}
+	ss := &mbrs
+	var mbr_buf bytes.Buffer
+	binary.Write(&mbr_buf, binary.BigEndian, ss)
+	WriteNextBytes(file, mbr_buf.Bytes())
 	file.Seek(file_size,0)
+
 	var second_buffer bytes.Buffer
 	binary.Write(&second_buffer, binary.BigEndian, s)
 	WriteNextBytes(file, second_buffer.Bytes())
+	file.Seek(0,0)
+	
+}
+
+func WriteBFile(file_path string, rec mbr, pos int){
+	file, err := os.Create(file_path)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("Cannot write the file")
+	}
+	file.Seek(0, pos)
+	ss := &rec
+	var mbr_buf bytes.Buffer
+	binary.Write(&mbr_buf, binary.BigEndian, ss)
+	WriteNextBytes(file, mbr_buf.Bytes())
+	file.Seek(0, 0)
 }
 
 func WriteNextBytes(file *os.File, bytes []byte) {
@@ -57,4 +130,8 @@ func WriteNextBytes(file *os.File, bytes []byte) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func generateRandom() int8{
+	return int8(rand.Int())
 }
---
+++ function/disk.go
@@ -0,0 +1,209 @@
+package function
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"os"
+)
+
+var Equalizer string = "->"
+var PartitionError bool = false
+
+type binaryFile struct {
+	size int
+	path string
+	name string
+	unit string
+}
+
+type partition_config struct{
+	Size int8
+	Unit byte
+	Path string
+	Type byte
+	Fit[1] byte
+	Delete bool
+	Name string
+	Add bool
+}
+
+
+func Exec_fdisk(com []string) {
+	var new_partition partition_config
+	for _, element := range com {
+		spplited_command := strings.Split(element, Equalizer)
+		switch strings.ToLower(spplited_command[0]) {
+		case "-size":
+			i, _ := strconv.Atoi(spplited_command[1])
+			if i > 0 {
+				new_partition.Size = int8(i)
+				//fmt.Println("Partition size ",new_partition.Size)
+			} else {
+				fmt.Println("Partition size must be positive")
+				return
+			}
+		case "-unit":
+			new_partition.Unit = spplited_command[1][0]
+			//fmt.Println("Partition unit",new_partition.Unit)
+		case "-path":
+			if _, err := os.Stat(spplited_command[1]); !os.IsNotExist(err) {
+				new_partition.Path = spplited_command[1]
+				//fmt.Println("Disk path",new_partition.Path )
+			}else{
+				fmt.Println("Especificated disk doesnt exist")
+				return
+			}
+		case "-type":
+			new_partition.Type =  strings.ToLower(spplited_command[1])[0]
+			//fmt.Println("Partition type", new_partition.Type)
+		case "-fit":
+			var fit_slice[1] byte
+			copy(fit_slice[:], strings.ToLower(spplited_command[1])) 
+			new_partition.Fit = fit_slice
+			//fmt.Println("Partition fit", fit_slice)
+		case "-delete":
+			new_partition.Delete = true
+		case "-name":
+			new_partition.Name = spplited_command[1]
+			//fmt.Println("Partition name",new_partition.Name)
+		case "-add":
+			new_partition.Add = true
+		default:
+			if spplited_command[0] != "fdisk" {
+				fmt.Println(spplited_command[0], "command unknow")
+			}
+		}
+	}
+	if new_partition.Unit == 0 {
+		new_partition.Unit = 'k'
+		fmt.Println("You didnt specify an unit size")
+	}
+	if(new_partition.Add && !new_partition.Delete){
+		
+	}else if(new_partition.Delete && !new_partition.Add){
+
+	}else if(!new_partition.Delete && !new_partition.Add){
+		record := ReadBinaryFile(new_partition.Path)
+		createPartition(&record, new_partition)
+		if(!PartitionError){
+			WriteBFile(new_partition.Path, record, 1)
+			printDisk(ReadBinaryFile(new_partition.Path))
+		}else{
+			PartitionError = false
+			return
+		}
+	}else {
+		fmt.Println("Incorrect params combination")
+	}
+}
+
+func createPartition(r *mbr, p partition_config){
+	par_unit := string(p.Unit)
+	disk_size := r.Size
+	part_size := calc_filesize(par_unit,int(p.Size), true)
+	for i:=0;i<len(r.Partitions);i++ {
+		if(r.Partitions[i].Status == '0'){
+			r.Partitions[i].Status = 'i'
+			r.Partitions[i].Type = p.Type
+			r.Partitions[i].Fit = p.Fit[0]
+			if(i==0){
+				r.Partitions[i].Start = 0
+			}else{
+				ps := int64(r.Partitions[i-1].Start) + r.Partitions[i-1].Size
+				if(int64(ps)<=disk_size){
+					r.Partitions[i].Start = ps
+				}else{
+					fmt.Println("Not enough space on disk")
+					PartitionError = true
+				}
+			}
+			r.Partitions[i].Size = part_size
+			var parN[16] byte
+			copy(parN[:],p.Name)
+			r.Partitions[i].Name = parN
+			return 
+		}	
+	}
+}
+
+func Exec_mrdisk(com []string) {
+	splitted_command := strings.Split(com[1], Equalizer)
+	if splitted_command[0] == "-path" {
+		file_name := splitted_command[1]
+		deleteFile(file_name)
+	} else {
+		fmt.Println(splitted_command[0], "command unknow")
+	}
+}
+
+func Exec_mkdisk(com []string) {
+	var new_disk binaryFile
+	for _, element := range com {
+		spplited_command := strings.Split(element, Equalizer)
+		switch strings.ToLower(spplited_command[0]) {
+		case "-size":
+			i, _ := strconv.Atoi(spplited_command[1])
+			if i > 0 {
+				new_disk.size = i
+			} else {
+				fmt.Println("Size must be positive! ")
+				return
+			}
+		case "-path":
+			if _, err := os.Stat(spplited_command[1]); os.IsNotExist(err) {
+				os.MkdirAll(spplited_command[1], os.ModePerm)
+			}
+			new_disk.path = spplited_command[1]
+		case "-name":
+			if strings.HasSuffix(spplited_command[1], ".dsk") {
+				new_disk.name = spplited_command[1]
+			} else {
+				fmt.Println("Error! Name must have .dsk extension")
+				return 
+			}
+		case "-unit":
+			new_disk.unit = spplited_command[1]
+		default:
+			if spplited_command[0] != "mkdisk" {
+				fmt.Println(spplited_command[0], "command unknow")
+			}
+		}
+	}
+	CreateBinaryFile(new_disk.name,new_disk.path, calc_filesize(new_disk.unit, new_disk.size,false))
+	filen := new_disk.path+ new_disk.name 
+	printDisk(ReadBinaryFile(filen))
+}
+
+
+func calc_filesize(unit string, size int, partition bool)int64{
+	if(unit=="" && !partition){
+		unit = "m"
+	}else if(unit=="" && partition){
+		unit = "k"
+	}
+	switch strings.ToLower(unit) {
+	case "k":
+		return 1024*int64(size)
+	case "m":
+		return 1024*1024*int64(size)
+	case "b":
+		return int64(size)
+	default:
+		fmt.Println("Invalid unit formmat")
+	}
+	return 0
+}
+
+func deleteFile(path string){
+	if _, err := os.Stat(path); !os.IsNotExist(err) {
+		err := os.Remove(path)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}	
+		fmt.Println("Removed successfully!")
+	}else{
+		fmt.Println("Error: File doesnt exists!")
+	}
+}
